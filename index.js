072622d2f9c07677376d4846e4cb2faf6f6e5a5b
Victoria <tapia.victoria5070@gmail.com>
2018-12-04 13:53:06 +0100

    fix node.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const handler = require('serve-handler');
 const http = require('http');
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((request, response) => {
     // You pass two more arguments for config and middleware
     // More details here: https://github.com/zeit/serve-handler#options
     return handler(request, response);
 });
 
-server.listen(3000, () => {
-    console.log('Running at http://localhost:3000');
+server.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
 });
